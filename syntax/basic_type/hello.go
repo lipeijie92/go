23535a77896bea475f9a56b8d3b64a01978b5dae
某某 <1733627701@qq.com>
2025-01-27 10:55:37 +0800

    添加go测试文件

---
+++ syntax/basic_type/hello.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"basic/syntax/string"
+)
+
+var sex = "男"
+var Dos = "Hello, string1"
+
+func main() {
+	/*println("Hello, World!")
+	println(sex)*/
+	/*println(string.Dos)
+	println(Dos)*/
+	string.String2()
+	println(string.Dos)
+
+}
---
+++ syntax/string/test1.go
@@ -0,0 +1,15 @@
+package string
+
+var Dos = "Hello, string"
+
+func String() {
+	println("Hello, string")
+}
+
+func String2() {
+	var name = "离谱"
+	name = "你好"
+	Dos = "你好 dos"
+	println(Dos)
+	println(name)
+}
